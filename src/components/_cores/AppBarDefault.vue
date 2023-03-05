f9d0b8fdfaa5a4ef79f165ccb68b92658a7ec9b0
Juan Pablo Spiatta <spiattajuanpablodesarrollador@gmail.com>
2023-03-05 20:11:48 -0300

    App layout created - app-bar and main content

---
+++ src/components/_cores/AppBarDefault.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+  <v-app-bar>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-title>JPS/ Vuetify 3</v-app-bar-title>
+    <template v-slot:append>
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+    </template>
+  </v-app-bar>
+</template>
---
+++ src/components/views/_layouts/AppLayoutDefault.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import AppBarDefault from "@/components/_cores/AppBarDefault.vue";
+</script>
+<template>
+  <AppBarDefault />
+  <v-main>
+    <slot name="main-content"></slot>
+  </v-main>
+</template>
